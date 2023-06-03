15b124bc4bf13366044364dce332e252f8d8e726
Isabella Forte <113397251+isabellaforte@users.noreply.github.com>
2023-06-03 20:35:44 -0300

    enum provincias

---
+++ .H Y .CPP/ePROVINCIA.h
@@ -0,0 +1,2 @@
+#pragma once
+enum ePROVINCIA{BuenosAires, CABA,Catamarca, Chaco, Chubut, Cordoba, Corrientes, EntreRios, Formosa, Jujuy, LaPampa, LaRioja, Mendoza, Misiones, Neuquen, RioNegro,Salta, SanJuan, SanLuis, SantaCruz, SantaFe, SantiagoDelEstero,TierraDelFuego,Tucuman };
